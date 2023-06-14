1ad80b5932094c60516a442eff6c2a272dcce358
joshua <josh.newbill08@hotmail.com>
2023-06-13 20:21:48 -0500

    Classes added and works

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+
+console.log("hello");
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+//const bfast = new Breakfast("eggs", "juice") ----> Testing
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+
+class Dinner {
+    #dessert
+
+    constructor(salad,soup,entree,dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+
+}
